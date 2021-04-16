bb5f5c4925f15fa6317c6bff36efc30a75370da8
Vimal Bharti <s.vimalbhart@gmail.com>
2021-04-16 19:37:38 +0530

    Add top to Button and our products effects

--- files/script.js
+++ files/script.js
@@ -5,4 +5,17 @@ function closeNav(){
     document.getElementById("mySidebar").style.width = "0";
 }
 
- 
+// Botton to Top
+var btn = $('#button-top');
+$(window).scroll(function() {
+    if($(window).scrollTop() > 400){
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+});
